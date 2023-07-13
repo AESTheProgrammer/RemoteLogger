cd62d4021f03d50b175e554dd9c067f20c91a59f
armin <armines.bin2000@gmail.com>
2023-07-13 11:24:15 +0330

    Add project files.

---
+++ RemoteLogger/RemoteLogger.cpp
@@ -0,0 +1,12 @@
+﻿// RemoteLogger.cpp : Defines the entry point for the application.
+//
+
+#include "RemoteLogger.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
